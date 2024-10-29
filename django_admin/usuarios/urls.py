b62237981049efc3cc112c6d2921aae77c727a92
ThDelgado <100494591+ThDelgado@users.noreply.github.com>
2024-10-29 10:35:00 -0300

    modificacion de login y mensajes

--- django_admin/usuarios/urls.py
+++ django_admin/usuarios/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = [
     path('registro/', views.registro_view, name="registro_view"),
     path('login/', views.login_view, name="login_view"),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
+
--- django_admin/usuarios/views.py
+++ django_admin/usuarios/views.py
@@ -1,5 +1,5 @@
 from django.forms import ValidationError
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
@@ -29,7 +29,7 @@ def registro_view(request):
             login(request, user)
             
             messages.success(request, f"Usuario {user.username} registrado con éxito.")
-            return HttpResponseRedirect("/")
+            return redirect('home')
         else:
             messages.error(request, "Error al intentar registrar al usuario, por favor verifique los datos.")
             return render(request, 'usuarios/registro.html', {"form": form})
@@ -44,19 +44,23 @@ def login_view(request):
     form = LoginUsuarioForm()
     
     if request.method == "POST":
-        form = LoginUsuarioForm[request.POST]
+        form = LoginUsuarioForm(request.POST)
 
         if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
-            user = User.objects.get(username= username, password= password)
+            user = authenticate(request, username= username, password= password)
          
             if user is not None:
                 login(request, user)
-                return redirect("success_home")
+                return redirect("home")
             
         else:
             form.add_error(None, "Formulario invalido, Por favor,  revise los campos")
 
     return render(request, 'usuarios/login.html', {'form': form})
 
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
